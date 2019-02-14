eb650afe8dbfee88f72bcfe29788f1297ec8aa61
bakatrouble <bakatrouble@gmail.com>
2019-02-14 06:53:14 +0300

    trying to fix ninja build

--- build_scripts/04-build.bat
+++ build_scripts/04-build.bat
@@ -1,9 +1,8 @@
 if "%PLATFORM%"=="x86" (
     set OPUS_PLATFORM=Win32
-    set NINJA_ENV=x64
 ) else (
     set OPUS_PLATFORM=x64
-    set NINJA_ENV=x86
+    set GYP_DEFINES=target_arch=x64
 )
 
 set BUILD_DEPS=c:\projects\build_deps
@@ -11,4 +10,4 @@ set PATH=%BUILD_DEPS%;%PATH%
 
 c:\python27\python.exe %BUILD_DEPS%\gyp_main.py -D OS=win -D opus_platform=%OPUS_PLATFORM% --depth=. --format ninja libtgvoip.gyp
 c:\python27\python.exe %BUILD_DEPS%\gyp_main.py -D OS=win -D opus_platform=%OPUS_PLATFORM% --depth=. --format msvs-ninja libtgvoip.gyp
-ninja -C out/Release -e out/Release/environment.%NINJA_ENV%
+ninja -C out/Release
