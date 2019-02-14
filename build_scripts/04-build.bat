dd95978fa6ead4722bbd64ce1b595f026f5f13cb
bakatrouble <bakatrouble@gmail.com>
2019-02-14 04:54:44 +0300

    fix build script

--- build_scripts/04-build.bat
+++ build_scripts/04-build.bat
@@ -2,8 +2,9 @@ set PROJECTS=c:\projects
 set BUILD_DEPS=%PROJECTS%\build_deps
 set PATH=%BUILD_DEPS%;%PATH%
 set PYTHON=c:\python27\python.exe
-set GYP="%PYTHON% %BUILD_DEPS%\gyp_main.py"
 
-%GYP% -D OS=win --depth=. --format ninja libtgvoip.gyp
-%GYP% -D OS=win --depth=. --format msvs-ninja libtgvoip.gyp
+doskey gyp_py = "%PYTHON% %BUILD_DEPS%\gyp_main.py"
+
+gyp_py -D OS=win --depth=. --format ninja libtgvoip.gyp
+gyp_py -D OS=win --depth=. --format msvs-ninja libtgvoip.gyp
 ninja -C out/Release
