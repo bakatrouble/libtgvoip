7465c5a1089e982a936c4d91d5cf545330680c80
bakatrouble <bakatrouble@gmail.com>
2019-02-14 06:12:27 +0300

    do not use prefixes

--- build_scripts/02-opus.bat
+++ build_scripts/02-opus.bat
@@ -2,10 +2,8 @@ cd ..
 
 if "%PLATFORM%"=="x86" (
     set BUILD_PLATFORM=Win32
-    set PREFIX=Release32
 ) else (
     set BUILD_PLATFORM=Win64
-    set PREFIX=Release64
 )
 
 if exist "opus\win32\VS2015\Win32\%PREFIX%\opus.lib" goto ALREADY_BUILT
@@ -15,7 +13,7 @@ git clone https://github.com/telegramdesktop/opus.git
 cd opus
 git checkout tdesktop
 cd win32\VS2015
-msbuild opus.sln /property:Configuration=Release /property:Platform="%BUILD_PLATFORM%" /property:OutputPath="%PREFIX%"
+msbuild opus.sln /property:Configuration=Release /property:Platform="%BUILD_PLATFORM%"
 cd ..\..\..
 goto FINISH
 
