0284dfd88461ad3d238aeec72aa183f1ee111361
bakatrouble <bakatrouble@gmail.com>
2019-02-14 03:52:03 +0300

    use curl

--- win_build.sh
+++ win_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget -nc https://www.nuget.org/api/v2/package/openssl-vc141-static-x86_64/1.1.0 -O openssl.zip
+curl -fsS https://www.nuget.org/api/v2/package/openssl-vc141-static-x86_64/1.1.0 -o openssl.zip
 unzip openssl.zip 'build/native/*' -d openssl && mv openssl/build/native/* openssl && rm -rf openssl/build
 
 echo "Building Opus..."
