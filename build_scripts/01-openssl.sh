767de9c12df11b33c72065b27dca53f759dec6af
bakatrouble <bakatrouble@gmail.com>
2019-02-14 03:58:50 +0300

    reupload openssl

--- build_scripts/01-openssl.sh
+++ build_scripts/01-openssl.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Downloading openssl..."
-curl -fsS https://www.nuget.org/api/v2/package/openssl-vc141-static-x86_64/1.1.0 -o openssl.zip
-unzip openssl.zip 'build/native/*' -d openssl && mv openssl/build/native/* openssl && rm -rf openssl/build
+curl -fsS https://drop.bakatrouble.pw/_/openssl.tar.gz -o openssl.tar.gz
+tar -xf openssl.tar.gz
