fbf684413055f6d581ad7369f8a32294df415580
bakatrouble <bakatrouble@gmail.com>
2019-02-14 07:20:16 +0300

    fix x86 build

--- build_scripts/04-build.bat
+++ build_scripts/04-build.bat
@@ -1,13 +1,15 @@
 if "%PLATFORM%"=="x86" (
     set OPUS_PLATFORM=Win32
+    set GYP_DEFINES=target_arch=ia32
 ) else (
     set OPUS_PLATFORM=x64
     set GYP_DEFINES=target_arch=x64
 )
 
+set GYP_DEFINES=%GYP_DEFINES% OS=win opus_platform=%OPUS_PLATFORM%
 set BUILD_DEPS=c:\projects\build_deps
 set PATH=%BUILD_DEPS%;%PATH%
 
-c:\python27\python.exe %BUILD_DEPS%\gyp_main.py -D OS=win -D opus_platform=%OPUS_PLATFORM% --depth=. --format ninja libtgvoip.gyp
-c:\python27\python.exe %BUILD_DEPS%\gyp_main.py -D OS=win -D opus_platform=%OPUS_PLATFORM% --depth=. --format msvs-ninja libtgvoip.gyp
+c:\python27\python.exe %BUILD_DEPS%\gyp_main.py --depth=. --format ninja libtgvoip.gyp
+c:\python27\python.exe %BUILD_DEPS%\gyp_main.py --depth=. --format msvs-ninja libtgvoip.gyp
 ninja -C out/Release
